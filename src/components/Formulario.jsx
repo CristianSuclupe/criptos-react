db6812bfdfd24c24c66b76164b7d30317d9eedb2
Cristian021000 <N00148478@upn.pe>
2023-08-03 01:01:20 -0500

    v0.1

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+
+`
+const Formulario = () => {
+
+    const [SelectMonedas] = useSelectMonedas('hola')
+
+    return (
+        <form>
+            <SelectMonedas />
+            <InputSubmit type='submit' value='cotizar' />
+        </form>
+    )
+}
+
+export default Formulario
