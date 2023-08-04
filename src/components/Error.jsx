5f0b8f155f9565df54ecd50edd0a2b32bceb8d28
Cristian021000 <N00148478@upn.pe>
2023-08-04 15:05:30 -0500

    v0.2

---
+++ src/components/Error.jsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled"
+
+const MensajeError = styled.p`
+    background-color: #B7322C;
+    color: #FFF;
+    padding: 15px;
+    font-size: 22px;
+    text-transform: uppercase;
+    font-family: 'Lato', sans-serif;
+    font-weight: 700;
+    text-align: center;
+`
+const Error = ({children}) => {
+    return (
+        <MensajeError>
+            {children}
+        </MensajeError>
+    )
+}
+
+export default Error
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from "react"
 import styled from "@emotion/styled"
 import useSelectMonedas from "../hooks/useSelectMonedas"
+import { MONEDAS } from "../constants/monedas"
+import Error from "./Error"
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -12,22 +15,54 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
-
+    margin-top: 30px;
     &:hover {
         background-color: #7A7DFE;
         cursor: pointer;
     }
 
 `
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
 
-    const [SelectMonedas] = useSelectMonedas('hola')
+    const [criptos, setCriptos] = useState([])
+    const [error, setError] = useState(false)
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', MONEDAS)
+    const [criptomoneda, SelectCripto] = useSelectMonedas('Elige tu criptomoneda', criptos)
+    
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+            const arrayCriptos = resultado.Data.map( cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name, 
+                    nombre: cripto.CoinInfo.FullName
+                }
+                
+                return objeto;
+            })
+            setCriptos(arrayCriptos);  
+        }
+        consultarAPI()
+    },[])
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(moneda, criptomoneda);
+        [moneda, criptomoneda].includes('') ? setError(true) : setError(false), setMonedas({moneda, criptomoneda})
+    }
     return (
-        <form>
-            <SelectMonedas />
-            <InputSubmit type='submit' value='cotizar' />
-        </form>
+        <>
+            {error && <Error>Todos los campos son obligatorios</Error>}
+            <form 
+                onSubmit={handleSubmit}
+            >
+                <SelectMonedas />
+                <SelectCripto />
+                <InputSubmit type='submit' value='cotizar' />
+            </form>
+        </>
     )
 }
 
